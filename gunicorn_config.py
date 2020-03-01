26f08fac4ecb19915c838772baa7b774afa8841d
Andrew Carl <andy@Andrews-MacBook-Air.local>
2020-02-29 20:43:15 -0700

    changed gunicorn user

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
-command = '/usr/local/bin/gunicorn'
-pythonpath = '/home/django/ThirstySamurai'
+command = '/webapps/samurai/bin/gunicorn'
+pythonpath = '/home/ThirstySamurai'
 bind = '127.0.0.1:8001'
 workers = 3
 user = 'nobody'
